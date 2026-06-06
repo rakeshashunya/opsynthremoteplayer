c43347be40fa214e992ced131bd0cd99ac2354bb
Ashunya, Inc. <rakesh@ashunya.com>
2026-06-05 18:54:24 -0700

    feat: make remote player stateless (do not save previous sessions or last remote ID)

--- apply-branding.py
+++ apply-branding.py
@@ -40,6 +40,119 @@ def main():
         (
             'pub static ref APP_NAME: RwLock<String> = RwLock::new("RustDesk".to_owned());',
             'pub static ref APP_NAME: RwLock<String> = RwLock::new("OpsynthRemotePlayer".to_owned());'
+        ),
+        # Remove unused import encrypt_vec_or_original
+        (
+            '''    password_security::{
+        decrypt_str_or_original, decrypt_vec_or_original, encrypt_str_or_original,
+        encrypt_vec_or_original, symmetric_crypt,
+    },''',
+            '''    password_security::{
+        decrypt_str_or_original, decrypt_vec_or_original, encrypt_str_or_original,
+        symmetric_crypt,
+    },'''
+        ),
+        # Disable storing of peer configurations to disk (no recent sessions saved)
+        (
+            '''    fn store_(&self, id: &str) {
+        let mut config = self.clone();
+        config.password =
+            encrypt_vec_or_original(&config.password, PASSWORD_ENC_VERSION, ENCRYPT_MAX_LEN);
+        for opt in ["rdp_password", "os-username", "os-password"] {
+            if let Some(v) = config.options.get_mut(opt) {
+                *v = encrypt_str_or_original(v, PASSWORD_ENC_VERSION, ENCRYPT_MAX_LEN)
+            }
+        }
+        if let Err(err) = store_path(Self::path(id), config) {
+            log::error!("Failed to store config: {}", err);
+        }
+        NEW_STORED_PEER_CONFIG.lock().unwrap().insert(id.to_owned());
+    }''',
+            '''    fn store_(&self, _id: &str) {
+        // Do not store any peer configs (connection history/settings) to disk
+    }'''
+        ),
+        # Disable reading of peer configurations (recent sessions list is empty)
+        (
+            '''    pub fn get_vec_id_modified_time_path(
+        id_filters: &Option<Vec<String>>,
+    ) -> Vec<(String, SystemTime, PathBuf)> {
+        if let Ok(peers) = Config::path(PEERS).read_dir() {
+            let mut vec_id_modified_time_path = peers
+                .into_iter()
+                .filter_map(|res| match res {
+                    Ok(res) => {
+                        let p = res.path();
+                        if p.is_file()
+                            && p.extension().map(|p| p.to_str().unwrap_or("")) == Some("toml")
+                        {
+                            Some(p)
+                        } else {
+                            None
+                        }
+                    }
+                    _ => None,
+                })
+                .map(|p| {
+                    let id = p
+                        .file_stem()
+                        .map(|p| p.to_str().unwrap_or(""))
+                        .unwrap_or("")
+                        .to_owned();
+
+                    let id_decoded_string = if id.starts_with("base64_") && id.len() != 7 {
+                        let id_decoded =
+                            base64::decode(&id[7..], base64::Variant::Original).unwrap_or_default();
+                        String::from_utf8_lossy(&id_decoded).as_ref().to_owned()
+                    } else {
+                        id
+                    };
+                    (id_decoded_string, p)
+                })
+                .filter(|(id, _)| {
+                    let Some(filters) = id_filters else {
+                        return true;
+                    };
+                    filters.contains(id)
+                })
+                .map(|(id, p)| {
+                    let t = crate::get_modified_time(&p);
+                    (id, t, p)
+                })
+                .collect::<Vec<_>>();
+            vec_id_modified_time_path.sort_unstable_by(|a, b| b.1.cmp(&a.1));
+            vec_id_modified_time_path
+        } else {
+            vec![]
+        }
+    }''',
+            '''    pub fn get_vec_id_modified_time_path(
+        _id_filters: &Option<Vec<String>>,
+    ) -> Vec<(String, SystemTime, PathBuf)> {
+        vec![]
+    }'''
+        ),
+        # Disable saving and reading the latest used remote ID
+        (
+            '''    pub fn set_remote_id(remote_id: &str) {
+        let mut config = LOCAL_CONFIG.write().unwrap();
+        if remote_id == config.remote_id {
+            return;
+        }
+        config.remote_id = remote_id.into();
+        config.store();
+    }
+
+    pub fn get_remote_id() -> String {
+        LOCAL_CONFIG.read().unwrap().remote_id.clone()
+    }''',
+            '''    pub fn set_remote_id(_remote_id: &str) {
+        // Do not store the last used remote ID
+    }
+
+    pub fn get_remote_id() -> String {
+        "".to_owned()
+    }'''
         )
     ]
 
